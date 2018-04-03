8dd9dfddf57f4f7ee7b4206923ba224b321c5018
Martin <ka_hei_martin_kwok@brown.edu>
2018-04-03 16:26:37 +0200

    Use rebin histograms

--- test/CA15_SF.py
+++ test/CA15_SF.py
@@ -1,12 +1,31 @@
 import os
+import numpy as np
 from ROOT import *
 from tabulate import tabulate
+import argparse
 #print tabulate(chi2Table,"firstrow")
 def errAB(product,a,errA,b,errB):
     ferr    = ((errA/a)**2+(errB/b)**2)**(0.5)
     return product * ferr
+def GetSFtableRow( SFname,  SFdict, normSFdict ):
+    row=[SFname]
+    row.append('%s'        %(SFdict['chi2_noTag']))
+    row.append('%s'        %(SFdict['chi2_Tag']))
+    row.append('%.3f +- %.3f'%(SFdict['effMC'],SFdict['effMC_err']))
+    row.append('%.3f +- %.3f'%(SFdict['effData'],SFdict['effData_err']))
+    row.append('%.3f +- %.3f'%(SFdict['sf'],SFdict['sf_err']))
+    if(SFname is not "Nominal"):
+        deltaSF = SFdict['sf'] - normSFdict['sf']
+        row.append('%.4f'%(deltaSF))
+    else:
+        row.append(" - ")
+    return row
 
-def FitAndGetEff(cf,b_template,all_templates,printOut=None):
+    
+
+def FitAndGetEff(cf,b_template,all_templates,nStat,SysName,printOut=None):
+    if nStat is not -1 :        cf.SetStatVariation(nStat)
+    if SysName is not "NA" :        cf.SetSysVariation(SysName)
     print "Running fit on untagged histograms"
     cf.Run()
     #printResults(cf,all_templates)
@@ -41,7 +60,10 @@ def FitAndGetEff(cf,b_template,all_templates,printOut=None):
         template_results.append(tmp_result)
     fitTable_row =['Inclusive',chi2pDOF_str,ndata,mc_sum] 
     fitTable.append(fitTable_row)
+
     print "Running fit on tagged histograms"
+    if nStat is not -1 :        cf.SetStatVariation(nStat)
+    if SysName is not "NA" :        cf.SetSysVariation(SysName)
     cf.Run("tag")   
     chi2pDOF = cf.GetChisq()
     Ndof = cf.GetNDOF()
@@ -91,7 +113,7 @@ def FitAndGetEff(cf,b_template,all_templates,printOut=None):
     if(printOut is not "noPrint"):
         print tabulate(resultTable,"firstrow") 
         print tabulate(fitTable,"firstrow")
-    return {"effMC":effMC,"errData":effData,"sf":sf,"effMC_err":effMC_err,"effData_err":effData_err,"sf_err":sf_err,"chi2_noTag":fitTable[0][1],"chi2_Tag":fitTable[0][1]} 
+    return {"effMC":effMC,"effData":effData,"sf":sf,"effMC_err":effMC_err,"effData_err":effData_err,"sf_err":sf_err,"chi2_noTag":fitTable[1][1],"chi2_Tag":fitTable[2][1]} 
 
 def printResults(cfit,templates):
     postFit_table=[]
@@ -120,10 +142,14 @@ def SetDataAndTemplates(cf,pTbin,WP,templates,glueTemplates=None,glueTemplatesTa
     tFile = TFile.Open(CFITinput)
     tagList = ["all_"+pTbin,WP+"pass_"+pTbin,WP+"fail_"+pTbin]
     histoNames =[]
-    #dataHeader = "UNWEIGHTED__DATA__FatJet_JP_"
-    #QCDheader  = "UNWEIGHTED__QCD__FatJet_JP_"
-    dataHeader = "DATA__FatJet_JP_"
-    QCDheader  = "QCD__FatJet_JP_"
+    if doSYS:
+        dataHeader = "UNWEIGHTED__DATA__FatJet_JP_"
+        QCDheader  = "UNWEIGHTED__QCD__FatJet_JP_"
+    else:
+        #dataHeader = "UNWEIGHTED__DATA__FatJet_JP_"
+        #QCDheader  = "UNWEIGHTED__QCD__FatJet_JP_"
+        dataHeader = "DATA__FatJet_JP_"
+        QCDheader  = "QCD__FatJet_JP_"
     for tag in tagList :
         if "all" in tag:
             dataHistoName = dataHeader+tag+"_data"
@@ -176,10 +202,17 @@ def SetDataAndTemplates(cf,pTbin,WP,templates,glueTemplates=None,glueTemplatesTa
         print hname
     print tabulate(preFit_yield,"firstrow")
 
+#parser = argparse.ArgumentParser()
+#parser.add_argument("--inputFile", help="path to the histogram file")
+#parser.add_argument("--doSYS"    , help="Run systematic variation")
+#parser.add_argument("--doStat"   ,  help="Run statistic variation")
+#args = parser.parse_args()
+doSYS  =False 
+doStat =True
+
 gInterpreter.Declare("#include \"RecoBTag/CFIT/interface/cfit.h\"")
 gSystem.Load(os.path.expandvars("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginRecoBTagCFIT.so"))
 cf = CFIT.cfit("JP discriminator")
-cf.SetLegendHeader("pT=[350,2000] DoubleBL=0.75")
 cf.SetOptimization(OPT_MORPH_SGN_SIGMA)
 cf.SetMorphing(OPTMORPH_GEOMETRIC)
 cf.ProducePlots(1)
@@ -189,7 +222,11 @@ cf.ProducePlots(1)
 #CFITinput = "/afs/cern.ch/user/k/kakwok/work/public/Hbb_ISR/CMSSW_8_0_23/src/RecoBTag/BTagValidation/test/CFitInput/CFIT_mcJPcalib.root"
 #CFITinput = "/afs/cern.ch/work/b/bmaier/public/MonoHiggs/BTV/CMSSW_8_0_23/src/RecoBTag/BTagValidation/test/Mu_350_merged/Final_histograms_sysMerged.root"
 #CFITinput = "/afs/cern.ch/work/b/bmaier/public/MonoHiggs/BTV/CMSSW_8_0_23/src/RecoBTag/BTagValidation/test/Mu_350_merged/Final_histograms_btagval.root"
-CFITinput = "/Final_histograms_btagval.root"
+#CFITinput = args.inputFile 
+#CFITinput = "Final_histograms_sysMerged.root"
+#CFITinput = "Final_histograms_sysMerged_rebinned.root"
+CFITinput = "CFIT_btagval_histograms_fixQCDnorm_rebin.root"
+#CFITinput  = "/afs/cern.ch/work/b/bmaier/public/MonoHiggs/BTV/CMSSW_8_0_23/src/RecoBTag/BTagValidation/test/SFtemplates_dataJPcalib/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTAK8Jet300_mcJPcalib_pt350_DoubleBM2.root"
 
 sysList = [
     'BFRAG',
@@ -200,28 +237,38 @@ sysList = [
     'PU'
 ]
 templates =[
-{'label':'bfromg', 'suffix':'bfromg','color':2},
-{'label':'b'     , 'suffix':'b'     ,'color':3},
-{'label':'cfromg', 'suffix':'cfromg','color':5},
-{'label':'c'     , 'suffix':'c'     ,'color':4},
-{'label':'l'     , 'suffix':'l'     ,'color':6}
+{'label':'g #rightarrow b#bar{b}', 'suffix':'bfromg','color':2},
+{'label':'b'                     , 'suffix':'b'     ,'color':3},
+{'label':'g #rightarrow c#bar{c}', 'suffix':'cfromg','color':5},
+{'label':'c'                     , 'suffix':'c'     ,'color':4},
+{'label':'l'                     , 'suffix':'l'     ,'color':6}
 ]
 glueTemplates=[
-{'label':'b+cfromg'  , 'glueList':['b','cfromg']  ,'color':221},
-{'label':'c+light'   , 'glueList':['c','l']       ,'color':93}
+{'label':'b+g #rightarrow c#bar{c}'  , 'glueList':['b','g #rightarrow c#bar{c}']  ,'color':221},
+{'label':'c+light'                   , 'glueList':['c','l']                       ,'color':93}
 ]
 glueTemplatesTag=[
-{'label':'others'  , 'glueList':['b','c','l','cfromg']  ,'color':42},
+{'label':'other flavours'  , 'glueList':['b','c','l','g #rightarrow c#bar{c}']  ,'color':42},
 ]
 pTbin = "pt350to2000"
 #pTbin = "pt200to350"
-WP    = "DoubleBM2"   # Benedikt uses M2 for 0.75
-#WP    = "DoubleBH"   # Benedikt uses H for 0.9
+#WP    = "DoubleBM2"   # Benedikt uses M2 for 0.75
+WP    = "DoubleBH"   # Benedikt uses H for 0.9
+cf.SetLegendHeader("pT=[350,2000] DoubleBL=0.9")
 print "Using input file: ",CFITinput
 
+sfTable =[]
+sfTable_cols=['Name','chi2(noTag)','chi2(Tag)','effMC','effData','sf','delta_SF']
+sfTable.append(sfTable_cols)
 
 cf.SetInputFile(CFITinput)
-cf.AddSys("Bfrag","_BFRAG_down","_BFRAG_up")
+if doSYS:
+    cf.AddSys("bFrag","_BFRAGUP","_BFRAGDOWN")
+    cf.AddSys("PU","_PUUP","_PUDOWN")
+    cf.AddSys("CD","_CD","_CD")
+    cf.AddSys("CFRAG","_CFRAG","_CFRAG")
+    cf.AddSys("K0L","_K0L","_K0L")
+    cf.AddSys("Ntrks","_NTRACKS","_NTRACKS")
 
 cf.SetMatrixOption("WRITE")
 # Set the data and QCD templates 
@@ -229,38 +276,55 @@ SetDataAndTemplates(cf, pTbin, WP, templates,glueTemplates,glueTemplatesTag)
 #SetDataAndTemplates(cf, pTbin, WP, templates)
 
 #labels of templates counted to be efficient
-b_templates =['bfromg']
+b_template ="g #rightarrow b#bar{b}"
 # Fit and get efficiencies
-normSFdict=FitAndGetEff(cf, 'bfromg',templates)
+normSFdict  = FitAndGetEff(cf, b_template,templates,-1,"NA")
+sfTable.append( GetSFtableRow("Nominal",normSFdict,normSFdict) )
 
 # perform statistical variation
-print '-----------------------------'
-print 'Perform statistical variation'
-print '-----------------------------'
-cf.SetMatrixOption("READ")
-avgSF = 0
-nStat = 10
-for i in range(667,667+nStat):
-    cf.SetStatVariation(678)
-    if i==667+nStat:
-        cf.ProducePlots(1)   
-    sf_dict = FitAndGetEff(cf, 'bfromg',templates,"noPrint")
-    avgSF += float(sf_dict['sf'])
-print "Average SF = %.3f "% (avgSF/nStat)
-#cf.Run()
-#cf.SetStatVariation(667)
-#cf.Run("tag")
+if doStat:
+    print '-----------------------------'
+    print 'Perform statistical variation'
+    print '-----------------------------'
+    cf.SetMatrixOption("READ")
+    avgSF = 0
+    nStat = 100
+    SFarray =[]
+    #The random seed starts from 667
+    for i in range(667,667+nStat):
+        if i==667+nStat:
+            cf.ProducePlots(1)   
+        sf_dict = FitAndGetEff(cf, b_template,templates,i,"NA","noPrint")
+        SFarray.append(float(sf_dict['sf']))
+    npSF  = np.array(SFarray)
+    avgSF = np.mean(npSF)
+    sdSF = np.std(npSF)
+    
+    print "Average SF = %.3f , S.D. = %.3f"% (avgSF,sdSF)
+    sfTable.append(['Statistics',' - ', '-', '-','-','%.3f'%avgSF, '%.3f'%(sdSF)])
    
 #do the calculation of SF here again ....
-
 #perform systematic variation
-#cf.SetMatrixOption("READ")
-#cf.SetSysVariation("_sys1_up")
-#cf.Run()
-#printResults(cf)
-#cf.SetSysVariation("_sys1_up")
-#cf.Run("tag")
-#printResults(cf)
+if doSYS:
+    print '-----------------------------'
+    print 'Perform systematic variation'
+    print '-----------------------------'
+    cf.SetMatrixOption("READ")
+    SFdict  = FitAndGetEff(cf, b_template,templates,-1,"_BFRAGUP")
+    sfTable.append( GetSFtableRow("BFragUP",SFdict,normSFdict) )
+    SFdict  = FitAndGetEff(cf, b_template,templates,-1,"_BFRAGDOWN")
+    sfTable.append( GetSFtableRow("BFragDOWN",SFdict,normSFdict) )
+    SFdict  = FitAndGetEff(cf, b_template,templates,-1,"_PUUP")
+    sfTable.append( GetSFtableRow("PU up",SFdict,normSFdict) )
+    SFdict  = FitAndGetEff(cf, b_template,templates,-1,"_PUDOWN")
+    sfTable.append( GetSFtableRow("PU down",SFdict,normSFdict) )
+    SFdict  = FitAndGetEff(cf, b_template,templates,-1,"_CD")
+    sfTable.append( GetSFtableRow("CD",SFdict,normSFdict) )
+    SFdict  = FitAndGetEff(cf, b_template,templates,-1,"_K0L")
+    sfTable.append( GetSFtableRow("K0L",SFdict,normSFdict) )
+    SFdict  = FitAndGetEff(cf, b_template,templates,-1,"_NTRACKS")
+    sfTable.append( GetSFtableRow("NTrks",SFdict,normSFdict) )
 
+print tabulate(sfTable,"firstrow")
 del cf
 gApplication.Terminate()
